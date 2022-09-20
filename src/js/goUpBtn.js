ada249f719539406f11a4418f4ba5b3a2e32b888
KolyaKolyaKolyadnica <borkovskihnikolaj@gmail.com>
2022-09-20 21:06:23 +0300

    test 1

--- src/js/goUpBtn.js
+++
@@ -1,9 +0,0 @@
-import refs from "./refs";
-
-export function getGoUpBtn() {
-  if (window.pageYOffset > 200) {
-    refs.goUpBtn.classList.add("active");
-  } else {
-    refs.goUpBtn.classList.remove("active");
-  }
-}
